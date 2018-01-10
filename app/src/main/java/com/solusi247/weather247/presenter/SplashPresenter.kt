ba17e77ded3be13798f0d90f28b5e3ccfdc3587f
Stefanus Julianto <stefanusj3@gmail.com>
2018-01-10 12:41:31 +0700

    Update SplashPresenter.kt

--- app/src/main/java/com/solusi247/weather247/presenter/SplashPresenter.kt
+++ app/src/main/java/com/solusi247/weather247/presenter/SplashPresenter.kt
@@ -5,7 +5,7 @@ import com.solusi247.weather247.listener.SplashListener
 class SplashPresenter(val listener: SplashListener) {
 
     fun loadSplash() {
-        val duration = 2000L
+        val duration = 2500L
         listener.showSplashScreen(duration)
     }
 
